cfa71366af55dbdbeaf3fd6c5b0d3bdf436ca394
LebedevDV <dan_spb_ru@mail.ru>
2021-04-30 11:35:00 +0300

    start script for mysql

--- aggregate-script/aggregators/mysql_aggregator.py
+++ aggregate-script/aggregators/mysql_aggregator.py
@@ -1,13 +1,24 @@
 # pip install mysql-connector-python
 import mysql.connector
 
-db = mysql.connector.connect(
-            host="itmo.fatalist.tech",
-            user="root",
-            password="P@ssw0rd"
-        )
-repository = "?"
+try:
+    db = mysql.connector.connect(
+                host="itmo.fatalist.tech",
+                user="root",
+                password="P@ssw0rd"
+            )
 
-print(db)
-# TODO logic
-db.close()
+    cursor = db.cursor()
+
+    print("Connected to DB.", db)
+
+    # persons
+    cursor.execute("SELECT * FROM user")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+    cursor.close()
+    db.close()
+except Exception as e:
+    print("ERROR MYSQL:", e)
