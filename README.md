ada127f3062da00cfa22bfa49ec0156947aae3c7
Dimitry82 <dan_spb_ru@mail.ru>
2021-05-05 18:08:31 +0300

    fix connections mysql

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # database-course
+
+## necessary relations for python scripts
+<ul>
+<li>pip install mysql-connector-python</li>
+<li>pip install cx_Oracle</li>
+</ul>
+
+For using Oracle Database in project, please copy files instantclient in project https://download.oracle.com/otn_software/nt/instantclient/191000/instantclient-basic-windows.x64-19.10.0.0.0dbru.zip
--- aggregate-script/aggregators/mysql_aggregator.py
+++ aggregate-script/aggregators/mysql_aggregator.py
@@ -1,24 +1,57 @@
-# pip install mysql-connector-python
 import mysql.connector
+import cx_Oracle
 
+
+def connect_to_mysql():
+    result = mysql.connector.connect(
+        host="itmo.fatalist.tech",
+        user="root",
+        password="P@ssw0rd"
+    )
+    print("Connected to MySQL.")
+    return result
+
+
+def connect_to_awm():
+    cx_Oracle.init_oracle_client(lib_dir=r"../instantclient_19_10")
+    dsn = cx_Oracle.makedsn("itmo.fatalist.tech", 1522, service_name="xe")
+    result = cx_Oracle.connect(
+        user="sys",
+        password="oracle",
+        dsn=dsn,
+        encoding="UTF-8",
+        mode=cx_Oracle.SYSDBA,
+    )
+    print("Connected to Oracle AWM.")
+    return result
+
+
+cursor_awm, db_awm, cursor_mysql, db_mysql = None, None, None, None
+# main script
 try:
-    db = mysql.connector.connect(
-                host="itmo.fatalist.tech",
-                user="root",
-                password="P@ssw0rd"
-            )
+    db_mysql = connect_to_mysql()
+    db_awm = connect_to_awm()
 
-    cursor = db.cursor()
-
-    print("Connected to DB.", db)
+    cursor_mysql = db_mysql.cursor()
+    cursor_awm = db_awm.cursor()
 
     # persons
-    cursor.execute("SELECT * FROM user")
-    result = cursor.fetchall()
+    cursor_mysql.execute("SELECT * FROM laba.user")
+    result = cursor_mysql.fetchall()
     for row in result:
         print(row)
 
-    cursor.close()
-    db.close()
+
 except Exception as e:
-    print("ERROR MYSQL:", e)
+    print("ERROR MYSQL_AGGREGATOR:", e)
+finally:
+    if cursor_awm is not None:
+        cursor_awm.close()
+    if db_awm is not None:
+        db_awm.close()
+    print("Connection to Oracle AWM closed.")
+    if cursor_mysql is not None:
+        cursor_mysql.close()
+    if db_mysql is not None:
+        db_mysql.close()
+    print("Connection to MySQL closed.")
